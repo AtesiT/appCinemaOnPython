11e272631eee86685d70b5a21fab8b8fd37f6a4a
AtesiT <atesit@mail.ru>
2023-05-23 23:00:51 +0300

    update functions and add amount of money

--- main.py
+++ main.py
@@ -2,6 +2,55 @@ from tkinter import *
 from random import randint, choice
 from tkinter import messagebox
 
+stillfirstText = 3
+stillsecondText = 2
+
+def toChangeAmountOfTicketsAndChangeAmountMoneyFirstButton ():
+    value = enterFirstText.get()
+    intValue = int(value)
+    global stillfirstText
+    global stillsecondText
+    global amountOfMoney
+
+    if (intValue == 0):
+        stillfirstText = stillfirstText - intValue
+        messagebox.showinfo('Ошибка', ("Вы ничего не приобрели"))
+        enterFirstText.delete(0, 'end')
+    elif (intValue > stillfirstText + 1):
+        stillfirstText = stillfirstText - intValue
+        messagebox.showinfo('Ошибка', ("Столько билетов нет"))
+        enterFirstText.delete(0, 'end')
+    else:
+        stillfirstText = stillfirstText - intValue
+        amountOfMoney = amountOfMoney - intValue*priceForFirstFilm
+        print(amountOfMoney)
+        messagebox.showinfo('Успешно', ("Вы купили билет(ы)"))
+        enterFirstText.delete(0, 'end')
+        # onDisplayMoney.config(text=amountOfMoney)
+
+def toChangeAmountOfTicketsAndChangeAmountMoneySecondButton ():
+    value = enterFirstText.get()
+    intValue = int(value)
+    global stillfirstText
+    global stillsecondText
+    global amountOfMoney
+
+    if (intValue == 0):
+        stillsecondText = stillsecondText - intValue
+        messagebox.showinfo('Ошибка', ("Вы ничего не приобрели"))
+        secondEnterText.delete(0, 'end')
+    elif (intValue > stillfirstText + 1):
+        stillsecondText = stillsecondText - intValue
+        messagebox.showinfo('Ошибка', ("Столько билетов нет"))
+        secondEnterText.delete(0, 'end')
+    else:
+        stillsecondText = stillsecondText - intValue
+        amountOfMoney = amountOfMoney - intValue*priceForSecondFilm
+        print(amountOfMoney)
+        messagebox.showinfo('Успешно', ("Вы купили билет(ы)"))
+        secondEnterText.delete(0, 'end')
+
+
 # Каркас
 window = Tk()
 window.resizable(width=False, height=False)
@@ -10,7 +59,7 @@ window.geometry('520x360')
 window['bg'] = 'white'
 
 
-#Надписи
+#Названия фильмов
 firstText = Label(window, text = 'Чебурашка', font = ('Arial Bold', 15), fg = 'black', bg = 'white')
 firstText.place(x = 25, y = 10)
 
@@ -20,9 +69,9 @@ secondText.place(x = 25, y = 100)
 
 # Надписи для полей ввода
 
-#Сколько осталось билетов
-stillfirstText = 3
-stillsecondText = 2
+#Сколько осталось билетов у фильмов
+# stillfirstText = 3
+# stillsecondText = 2
 
 #Цена за фильмы
 priceForFirstFilm = 250
@@ -48,4 +97,16 @@ enterFirstText.place(x = 35, y = 60)
 secondEnterText = Entry(fg='black', width=25)
 secondEnterText.place(x = 35, y = 160)
 
+#Денег на карте
+amountOfMoney = 1000
+
+onDisplayMoney = Label(window, text = f"У вас на карте: {str(amountOfMoney)}", font = ('Arial Bold', 15), fg = 'black', bg = 'white')
+onDisplayMoney.place(x = 25, y = 200)
+
+# Кнопки
+btn = Button(window, text = 'Купить', command = toChangeAmountOfTicketsAndChangeAmountMoneyFirstButton, width= '10', height='1', fg='blue', bg = 'white' )
+btn.place(x = 310, y = 25)
+
+btn = Button(window, text = 'Купить', command = toChangeAmountOfTicketsAndChangeAmountMoneySecondButton, width= '10', height='1', fg='blue', bg = 'white' )
+btn.place(x = 310, y = 125)
 window.mainloop()
